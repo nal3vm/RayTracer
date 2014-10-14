54a5e719be1177a6f325cdf0c8877ac87f04fd4a
Nathan Looney <nal3vm@virginia.edu>
2014-10-14 19:52:32 -0400

    Fixed GetRay i hope.

--- Ray/rayScene.todo.cpp
+++ Ray/rayScene.todo.cpp
@@ -24,28 +24,36 @@ Ray3D RayScene::GetRay(RayCamera* camera,int i,int j,int width,int height){
 	double angle = camera->heightAngle;
 	Point3D p0 = camera->position;
 
+	// Calculate the Z position of the coordinate relative to the XY plane.
 	Point3D pBottom = p0 + camera->direction - camera->up*tan(angle);
 	Point3D pTop = p0 + camera->direction + camera->up*tan(angle);
 	Point3D pI = pBottom + (pTop-pBottom)*((double(i)+0.5)/double(height));
 
-
+	// Calculate the Y position of the coordinate relative to the XZ plane.
 	Point3D pRight = p0 + camera->direction - camera->right*tan(angle);
 	Point3D pLeft = p0 + camera->direction + camera->right*tan(angle);
-	Point3D pJ = pRight + (pLeft-pRight)*((double(i)+0.5)/double(height));
+	Point3D pJ = pRight + (pLeft-pRight)*((double(j)+0.5)/double(height));
 
+	// Map the pixel location on a plane 1 unit away.
 	Point3D pFinal(1, pJ[1], pI[2]);
 
-	Point3D vect = (camera->position-pFinal).unit();
+	// Normalize 
+	Point3D vect = pFinal - camera->position;
+	vect = vect.unit();
+
+	Ray3D ret;
+	ret.direction = vect;
+	ret.position = p0;
 
 	std::cout << "<";
-	std::cout << vect[0];
+	std::cout << ret.direction[0];
 	std::cout << ",";
-	std::cout << vect[1];
+	std::cout << ret.direction[1];
 	std::cout << ",";
-	std::cout << vect[2];
+	std::cout << ret.direction[2];
 	std::cout << ">";
 	std::cout << "\n";
-	return vect;
+	return Ray3D();
 }
 
 Point3D RayScene::GetColor(Ray3D ray,int rDepth,Point3D cLimit){
